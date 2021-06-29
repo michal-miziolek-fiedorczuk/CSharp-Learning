063f21b0f69b31728f30cc41083addd7f258eb7e
Michał <9918405+michal-miziolek-fiedorczuk@users.noreply.github.com>
2021-06-29 13:48:49 +0200

    update commit

---
+++ HttpRequest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Collections.Generic;
+
+namespace HttpRequest
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            await ProcessRepositories();
+        }
+
+        private static async Task ProcessRepositories()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            // var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            var stringTask = client.GetStringAsync("https://api-shipx-pl.easypack24.net/v1/tracking/637450638105070127332959");
+            var repositories = JsonSerializer.Deserialize<Repository>(await stringTask);
+
+                Console.WriteLine("Tracking number: " + repositories.TrackingNumber);
+                Console.WriteLine("Status: " + repositories.Status);
+                Console.WriteLine("Target machine: " + repositories.TargetMachineID);
+                //Console.WriteLine("Kurs: " + repositories.Rate);
+
+        }
+    }
+}
---
+++ HttpRequest/repo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace HttpRequest
+{
+    public class Repository
+    { 
+        [JsonPropertyName("tracking_number")]
+        public string TrackingNumber { get; set; }
+        [JsonPropertyName("status")]
+        public string Status { get; set; }
+        [JsonPropertyName("name")]
+        public string TargetMachineID { get; set; }
+        [JsonPropertyName("mid")]
+        public float Rate { get; set; }
+    }
+}
